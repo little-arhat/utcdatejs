198e00d8abd77449177ae76a4f9da58aaf8dc2aa
Roman Sokolov <sokolov.r.v@gmail.com>
2011-08-09 12:16:39 +0400

    init commit

---
+++ utcdate.js
@@ -0,0 +1,87 @@
+// UTCDate is simple class, which represents datetime in UTC.
+// It conforms standard Date api.
+// Thanks to Alexander Solovyov (piranha, solovyov.net) for his help
+// with this piece of code.
+
+// Public Domain.
+
+var UTCDate = (function () {
+
+    var method_mapping = {
+        getHours: 'getUTCHours',
+        getDate: 'getUTCDate',
+        getDay: 'getUTCDay',
+        getFullYear: 'getUTCFullYear',
+        getMilliseconds: 'getUTCMilliseconds',
+        getMinutes: 'getUTCMinutes',
+        getMonth: 'getUTCMonth',
+        getSeconds: 'getUTCSeconds',
+        toDateString: 'toUTCString',
+        toString: 'toUTCString',
+        getMonth: 'getUTCMonth',
+
+        setDate: 'setUTCDate',
+        setFullYear: 'setUTCFullYear',
+        setHours: 'setUTCHours',
+        setMilliseconds: 'setUTCMilliseconds',
+        setMinutes: 'setUTCMinutes',
+        setMonth: 'setUTCMonth',
+        setSeconds: 'setUTCSeconds'
+    }
+
+    // Methods, which are oresent in prototype, but somehow
+    // aren't visible during iteration
+    var additional_methods = {
+        getTime: null,
+        setTime: null
+    };
+
+    var UTCDate = function(date) {
+        if(date)
+            this.inner = new Date(date);
+        else
+            this.inner = new Date();
+    }
+
+    UTCDate.prototype.getTimezoneOffset = function(){
+        return 0;
+    }
+
+    for (var k in method_mapping) {
+        (function(key) {
+            UTCDate.prototype[key] = function() {
+                return this.inner[method_mapping[key]].apply(this.inner, arguments);
+            }
+        })(k);
+    }
+
+    for (var k in additional_methods) {
+        (function(key) {
+            UTCDate.prototype[key] = function() {
+                return this.inner[key].apply(this.inner, arguments);
+            }
+        })(k);
+    }
+
+    for (var k in Date.prototype) {
+        (function(key){
+            if (!UTCDate.prototype[key]) {
+                UTCDate.prototype[key] = function() {
+                    return this.inner[key].apply(this.inner, arguments);
+                }
+            }
+        })(k);
+    }
+
+    for (var k in Date) {
+        (function(key) {
+            if (!UTCDate.prototype[key]) {
+                UTCDate.prototype[key] = function() {
+                    return this.inner[key].apply(this.inner, arguments);
+                }
+            }
+        })(k);
+    }
+
+    return UTCDate;
+})();
